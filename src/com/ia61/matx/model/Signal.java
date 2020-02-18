5401e317181502a4645afd672ed80d7d074e541b
Stas Panasiuk <temnyip@gmail.com>
2020-02-18 15:44:29 +0200

    init

---
+++ src/com/ia61/matx/model/Signal.java
@@ -0,0 +1,5 @@
+package com.ia61.matx.model;
+
+public interface Signal {
+
+}
---
+++ src/com/ia61/matx/model/SignalImpl/AnalogSignal.java
@@ -0,0 +1,7 @@
+package com.ia61.matx.model.SignalImpl;
+
+import com.ia61.matx.model.Signal;
+
+public class AnalogSignal implements Signal {
+
+}
