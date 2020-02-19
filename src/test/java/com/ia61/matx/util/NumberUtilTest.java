771ec3df35dd9be4ff204f09d4d169df43faecdc
Stas Panasiuk <temnyip@gmail.com>
2020-02-19 18:26:57 +0200

    cleanup

--- src/test/java/com/ia61/matx/util/NumberUtilTest.java
+++ src/test/java/com/ia61/matx/util/NumberUtilTest.java
@@ -1,15 +1,13 @@
 package com.ia61.matx.util;
 
-import java.util.Arrays;
-
-import org.junit.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Arrays;
+import org.junit.Test;
 
 public class NumberUtilTest {
 
   @Test
-  public void shouldFindGCD() {
+  public void shouldFindGCDWithUsualValues() {
     assertThat(NumberUtil.findGCD(Arrays.asList(24L, 18L))).isEqualTo(6L);
     assertThat(NumberUtil.findGCD(Arrays.asList(10L, 30L))).isEqualTo(10L);
     assertThat(NumberUtil.findGCD(Arrays.asList(10L, 20L, 25L))).isEqualTo(5L);
