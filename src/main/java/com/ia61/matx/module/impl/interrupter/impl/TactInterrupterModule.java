f174dd451cc1c070fa578909a883c4b9f94514e5
Stas <36239821+iSan4eZ@users.noreply.github.com>
2020-04-05 11:31:42 +0300

    DecisionMaker module is done (#9)
    * DecisionMaker module is done
    
    * Added DecisionMaker to FE part
    
    * removed RESULT_LABEL

--- src/main/java/com/ia61/matx/module/impl/interrupter/impl/TactInterrupterModule.java
+++ src/main/java/com/ia61/matx/module/impl/interrupter/impl/TactInterrupterModule.java
@@ -2,7 +2,7 @@ package com.ia61.matx.module.impl.interrupter.impl;
 
 import com.ia61.matx.module.impl.interrupter.AbstractInterrupter;
 
-public class CorrelatorInterrupterModule extends AbstractInterrupter {
+public class TactInterrupterModule extends AbstractInterrupter {
 
   @Override
   public String getModuleName() {
